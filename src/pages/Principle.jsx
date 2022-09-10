736fc68e23b9bf919811a2ec9349807515ae7103
Solomon Akuson <akusonsolomon15@gmail.com>
2022-09-10 15:05:33 +0100

    reduced the fontsize for 'our priciple' page

--- src/pages/Principle.jsx
+++ src/pages/Principle.jsx
@@ -4,12 +4,12 @@ const Principle = () => {
     return (
 
         <>
-            <div className=" h-screen bg-no-repeat">
+            <div className=" h-screen md:bg-no-repeat">
             <Navbar />
 
             <h1 className="text-center mt-[5rem] py-5 font-semibold text-3xl">Our <span className="text-secondary">Principle</span></h1>
-            <div className="flex justify-center justify-center bg-img3 bg-opacity-0 bg-no-repeat bg-auto bg-left  ">
-                <p className="text-center w-2/3 text-black text-opacity-75 leading-10 text-3xl font-light pt-5 mt-5">
+            <div className="flex justify-center  md:bg-img3 bg-opacity-0 bg-no-repeat bg-auto bg-left  ">
+                <p className="text-center w-2/3 text-black text-opacity-75 text-lg leading-7 md:leading-10 pt-5 mt-5">
                     The Plateau State One Stop Investment Centre is Plateaus foremost Investment
                     attraction and promotion agency. <br />
                     “As part of our mandate, we facilitate valuable investments to stimulate economic
