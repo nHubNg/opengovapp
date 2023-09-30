7a300146dc552245d5e64e1e04e44029fb1d7f32
Desmond-Fon <desmondfon@yahoo.com>
2023-09-30 15:39:04 +0100

    changed base url

--- src/redux/feed/feed-comp.js
+++ src/redux/feed/feed-comp.js
@@ -1,7 +1,7 @@
 // import react from "react";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 // import axios from "axios";
-const baseurl = "https://opengovapi.herokuapp.com/api/v1"
+const baseurl = "https://ogp-api-service.onrender.com/api/v1";
 
 
 export const createFeed = createAsyncThunk(
